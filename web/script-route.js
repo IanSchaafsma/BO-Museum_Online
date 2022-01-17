6b92afb46e39cf4de005039e217ee8ebef383dac
Ian <33319@ma-web.nl>
2022-01-17 20:29:41 +0100

    bug fixes in head, Route styling/engels.

--- web/script-route.js
+++ web/script-route.js
@@ -4,54 +4,26 @@ const myInput = document.getElementById("myInput")
 
 let lokaties = [
     {
-        "titel":"plaats 0",
+        "titel":"0",
         "image":"img/FotoSlide1.jpg"
     },
 
     {
-        "titel":"plaats 1",
+        "titel":"1",
         "image":"img/FotoSlide2.jpg"
     },
     {
-        "titel":"plaats 2",
+        "titel":"2",
         "image":"img/FotoSlide3.jpg"
     },
     {
-        "titel":"plaats 3",
+        "titel":"3",
         "image":"img/FotoSlide4.jpg"
     },
     {
-        "titel":"plaats 4",
-        "image":"img/FotoSlide5.jpg"
-    },
-    {
-        "titel":"plaats 5",
-        "image":"img/FotoSlide6.jpg"
-    },
-    {
-        "titel":"plaats 6",
-        "image":"img/FotoSlide7.jpg"
-    },
-    {
-        "titel":"plaats 7",
-        "image":"img/FotoSlide8.jpg"
-    },
-    {
-        "titel":"plaats 8",
-        "image":"img/8.jpg"
-    },
-    {
-        "titel":"plaats 9",
-        "image":"img/9.jpg"
-    },
-    {
-        "titel":"plaats 10",
-        "image":"img/10.jpg"
-    },
-    {
-        "titel":"plaats 11",
-        "image":"img/11.jpg"
-    },
+        "titel":"4",
+        "image":"img/fotoSlide5.jpg"
+    }
 ]
 //myTitle.innerHTML = "dit is door het script toegevoegd"
 //myImage.src = "img/1.jpg"
